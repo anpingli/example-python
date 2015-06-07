d651476302be328ae618fe0059d4f600c170f860
anli <anli@redhat.com>
2015-06-07 23:15:59 +0800

    a

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM openshift/python-33-rhel7
 USER default
 EXPOSE 8080
 COPY . /opt/openshift/src/
+CMD ["./run.sh"]
--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,4 +1,4 @@
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Openshift V3 from gunicorn WSGI application!"]
+    return [b"Hello World from gunicorn WSGI application!"]
