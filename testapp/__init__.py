004fca817490920739a2b70f78a098d78ddddbd3
root <root@client.osev3-reliability.com.cn>
2015-06-10 23:11:58 +0800

    a

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,4 +1,4 @@
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Openshfit 941 V3from gunicorn WSGI application!"]
+    return [b"Hello World from gunicorn WSGI application!"]
