cf6c363008baa54366ee565809e5d1b853017dd8
anpingli <anli@redhat.com>
2015-06-15 21:07:12 +0800

    test

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,4 +1,4 @@
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Openshift  V3 from gunicorn WSGI application!"]
+    return [b"Openshfit 2123 V3 from gunicorn WSGI application!"]
